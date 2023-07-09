62aba29225c7517358763985e174586ab840deca
guilhermealves <guilhermeborgesti@gmail.com>
2023-07-09 17:54:00 -0300

    commit de finalizacao de chamada

--- src/main/kotlin/com/api/generatechatgpt/client/ChatGptClient.kt
+++ src/main/kotlin/com/api/generatechatgpt/client/ChatGptClient.kt
@@ -1,15 +1,19 @@
 package com.api.generatechatgpt.client
 
 import com.api.generatechatgpt.client.response.Response
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @FeignClient(name = "chat-gpt", url = "https://api.openai.com/v1/chat/completions")
 interface ChatGptClient {
-    @RequestMapping(method = [RequestMethod.POST], headers = ["content-type=application/json", "Authorization= Bearer seu token"])
-     fun callChatGpt(@RequestBody model : Model) : Response
+
+
+    @RequestMapping(method = [RequestMethod.POST], headers = ["content-type=application/json"])
+     fun callChatGpt(@RequestBody model : Model, @RequestHeader("Authorization") token : String) : Response
 
 
 }
--- src/main/kotlin/com/api/generatechatgpt/controller/ChatGptController.kt
+++ src/main/kotlin/com/api/generatechatgpt/controller/ChatGptController.kt
@@ -4,6 +4,7 @@ import com.api.generatechatgpt.client.ChatGptClient
 import com.api.generatechatgpt.client.Message
 import com.api.generatechatgpt.client.Model
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -18,14 +19,18 @@ class ChapGptController {
 
     val texto : String =  "gpt-3.5-turbo"
 
+    @Value("\${key}")
+    lateinit var key : String
 
     @GetMapping
     fun callChatGpt(@RequestParam text : String) : String?{
-        val response = chatGptClient.callChatGpt(mountBody(text))
+        val response = chatGptClient.callChatGpt(mountBody(text), "Bearer $key")
         val content = response.choices?.let {
             it.get(0).message?.content
         }
-        println(content)
+        var newStr = content?.split("```java")
+
+        println(newStr?.get(1))
         return content
     }
 
